0296b6efa428ec788eb27381ed0660d25d483386
nikita ananjevs <40365689+NikitaAnanjev@users.noreply.github.com>
2019-02-14 00:09:49 +0100

    Update texteditor.js

--- js/texteditor.js
+++ js/texteditor.js
@@ -1,9 +1,5 @@
 // SCRIPT FOR TEXT EDITOR
 
-// CKEDITOR.replace( 'short_descr');
-// CKEDITOR.replace( 'long_descr' );
-//
-
 var textarea = document.body.appendChild( document.createElement( 'textarea' ) );
 CKEDITOR.replace( textarea ,{
     language: 'da',
@@ -11,55 +7,3 @@ CKEDITOR.replace( textarea ,{
     uiColor: '#9AB8F3',
     toolbarCanCollapse: 'true'
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-// CKEDITOR.replace( 'short_descr',{
-//     language: 'da',
-//     height: '100',
-//     uiColor: '#9AB8F3',
-//     toolbarCanCollapse: 'true'
-// });
-//
-// CKEDITOR.replace( 'long_descr' ,{
-//     language: 'da',
-//     height: '100',
-//     uiColor: '#9AB8F3',
-//     toolbarCanCollapse: 'true'
-// });
-//
-// CKEDITOR.replace( 'short_descr_norw',{
-//     language: 'da',
-//     height: '100',
-//     uiColor: '#9AB8F3',
-//     toolbarCanCollapse: 'true'
-// });
-// CKEDITOR.replace( 'long_descr_norw',{
-//     language: 'da',
-//     height: '100',
-//     uiColor: '#9AB8F3',
-//     toolbarCanCollapse: 'true'
-// });
-// CKEDITOR.replace( 'short_descr_english',{
-//     language: 'da',
-//     height: '100',
-//     uiColor: '#9AB8F3',
-//     toolbarCanCollapse: 'true'
-// });
-// CKEDITOR.replace( 'long_descr_english',{
-//     language: 'da',
-//     height: '100',
-//     uiColor: '#9AB8F3',
-//     toolbarCanCollapse: 'true'
-// });
