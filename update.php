4e5d39c44bd8f5f3cc86694ecfb29e461af3dca3
Nikita Ananjevs <web.nikan@gmail.com>
2019-02-13 00:30:58 +0000

    finished.update.page

--- update.php
+++ update.php
@@ -9,22 +9,17 @@ include 'config/database.php';
 
 
 // READ DATA FROM DATABASE
-try{
+try {
 
+    $query = "SELECT * FROM products_description INNER JOIN products ON products.products_id = products_description.products_id WHERE products_description.products_id = '$id'";
+    $stmt = $con->prepare($query);
+    $stmt->execute();
 
+    // this is the first question mark
+    $stmt->bindParam(1, $id);
 
-    //    $query2 = "SELECT * FROM products INNER JOIN products_description ON products.products_id = products_description.products_id WHERE products_description.languages_id = 1  AND  products_description.products_id = '$id'";
-    $query2 = "SELECT * FROM products_description INNER JOIN products ON products.products_id = products_description.products_id WHERE products_description.products_id = '$id'";
-    $stmt2 = $con->prepare($query2);
-    $stmt2->execute();
-
-
-
-//    $row = $stmt2->fetch(PDO::FETCH_ASSOC);
-
-    // Execute the query
-
-    while ($row = $stmt2->fetch(PDO::FETCH_ASSOC)){
+    // Execute SELECT query
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         extract($row);
 
         //DK
@@ -32,6 +27,8 @@ try{
             $name = $row['products_description_name'];
             $description = $row['products_description_short_description'];
             $descriptionLong = $row['products_description_description'];
+            $description_id = $row['products_description_id'];
+
         }
 
         //NO
@@ -47,102 +44,76 @@ try{
             $description_no = $row['products_description_short_description'];
             $descriptionLong_no = $row['products_description_description'];
         }
-
     }
+
+    // this is the first question mark
+//    $stmt->bindParam(1, $id);
     $price = $products_price;
     $reference = $products_reference;
 
 
-
-
-}
-// show error
-catch(PDOException $exception){
+} // show error
+catch (PDOException $exception) {
     die('ERROR: ' . $exception->getMessage());
 }
 
 
-
-try {
-
-//    $query = "UPDATE products_description
-//            JOIN products
-//            ON products_description.products_id = products.products_id
-//            SET products_description_name = :name,
-//                    products_description_short_description = :short_descr,
-//                    products_description_description = :long_descr,
-//                    products_reference=:refer,
-//                    products_price=:price
-//                    WHERE products_description_id = :id";
-
-//      UPDATE MAIN INFORMATION
-    $query = "UPDATE products_description
-            JOIN products
-            ON products_description.products_id = products.products_id
-            SET products_description_name = :name,
-                    products_description_short_description = :short_descr,
-                    products_description_description = :long_descr,
-                    products_reference = :refer,
-                    products_price = :price
-                    WHERE languages_id = 1 AND products_id = :id";
-    $stmt = $con->prepare($query);
+//UPDATE DATABASE
+if ($_POST) {
+//if (isset($_POST['update_btn'])) {
+    try {
 
 
+        $query = "UPDATE
+    products_description
+JOIN products ON products_description.products_id = products.products_id
+SET
+    products_description_name = :name,
+    products_description_short_description =:short_descr,
+    products_description_description = :long_descr,
+    products.products_reference = :refer,
+    products.products_price =:price
+WHERE
+    products_description.languages_id = '1' AND products_description.products_id = :id;";
+
+
+        $stmt = $con->prepare($query);
 
 //      UPDATE ENGLISH DATA
 
-    $query_eng = "UPDATE products_description
-            SET products_description_name = :name_en,
-                    products_description_short_description = :short_descr_en,
-                    products_description_description = :long_descr_en
-                    WHERE products_description.languages_id = 2 AND products_id = :id";
-    $stmt_en = $con->prepare($query_eng);
+        $query_eng = "UPDATE
+    products_description
+SET
+    products_description_name=:name_en,
+    products_description_short_description=:short_descr_en,
+    products_description_description=:long_descr_en
+WHERE
+    products_description.languages_id = '2' AND products_id=:id";
+        $stmt_en = $con->prepare($query_eng);
+
+//
+//////      UPDATE NORWEGIAN
+////
+        $query_nor = "UPDATE
+    products_description
+SET
+    products_description_name=:name_no,
+    products_description_short_description=:short_descr_no,
+    products_description_description=:long_descr_no
+WHERE
+    products_description.languages_id = '3' AND   products_description.products_id=:id";
 
 
+        $stmt_no = $con->prepare($query_nor);
 
-//      UPDATE NORWEGIAN
-
-    $query_nor = "UPDATE products_description
-            SET products_description_name = :name_no,
-                    products_description_short_description = :short_descr_no,
-                    products_description_description = :long_descr_no
-            WHERE products_id = :id  AND products_description.languages_id = 3";
-    $stmt_no = $con->prepare($query_nor);
-
-
-
-
-
-//Dansk
-    $stmt->bindParam(':name', $name);
-    $stmt->bindParam(':short_descr', $description);
-    $stmt->bindParam(':long_descr', $descriptionLong);
-    $stmt->bindParam(':price', $price);
-    $stmt->bindParam(':refer', $reference);
-    $stmt->bindParam(':id', $id);
-
-//english
-    $stmt_en->bindParam(':name_en', $name_eng);
-    $stmt_en->bindParam(':short_descr_en', $description_eng);
-    $stmt_en->bindParam(':long_descr_en', $descriptionLong_eng);
-
-//norge
-    $stmt_no->bindParam(':name_no', $name_no);
-    $stmt_no->bindParam(':short_descr_no', $description_no);
-    $stmt_no->bindParam(':long_descr_no', $descriptionLong_no);
-
-
-
-
-
-    // Update product
-    if (isset($_POST['update_btn']) && $stmt2->execute()) {
-        $name = htmlspecialchars(strip_tags($_POST['name']));
-        $description = htmlspecialchars(strip_tags($_POST['short_descr']));
-        $descriptionLong = htmlspecialchars(strip_tags($_POST['long_descr']));
+        //BIND PARAMETERS FOR UPDATE
 
         $price = htmlspecialchars(strip_tags($_POST['price']));
         $reference = htmlspecialchars(strip_tags($_POST['refer']));
+
+        $name = htmlspecialchars(strip_tags($_POST['name']));
+        $description = htmlspecialchars(strip_tags($_POST['short_descr']));
+        $descriptionLong = htmlspecialchars(strip_tags($_POST['long_descr']));
 
         $name_eng = htmlspecialchars(strip_tags($_POST['name_english']));
         $description_eng = htmlspecialchars(strip_tags($_POST['short_descr_english']));
@@ -152,25 +123,49 @@ try {
         $description_no = htmlspecialchars(strip_tags($_POST['short_descr_norw']));
         $descriptionLong_no = htmlspecialchars(strip_tags($_POST['long_descr_norw']));
 
+        //Dansk
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':short_descr', $description);
+        $stmt->bindParam(':long_descr', $descriptionLong);
+        $stmt->bindParam(':price', $price);
+        $stmt->bindParam(':refer', $reference);
+        $stmt->bindParam(':id', $id);
+
+        //english
+        $stmt_en->bindParam(':name_en', $name_eng);
+        $stmt_en->bindParam(':short_descr_en', $description_eng);
+        $stmt_en->bindParam(':long_descr_en', $descriptionLong_eng);
+        $stmt_en->bindParam(':id', $id);
+
+        //norge
+        $stmt_no->bindParam(':name_no', $name_no);
+        $stmt_no->bindParam(':short_descr_no', $description_no);
+        $stmt_no->bindParam(':long_descr_no', $descriptionLong_no);
+        $stmt_no->bindParam(':id', $id);
+
 
         $stmt->execute();
         $stmt_en->execute();
         $stmt_no->execute();
 
-        // echo $stmt;
-        if ($stmt->execute()  ) {
+
+//        if ($stmt->execute()) {
+        if ($stmt->execute() && $stmt_en->execute() && $stmt_no->execute()) {
+
             echo "<div class='alert alert-success'>Record was updated.</div>";
         } else {
             echo "<div class='alert alert-danger'>Unable to update record. Please try  again.</div>";
-
-
         }
+    } // show error
+    catch (PDOException $exception) {
+        die('ERROR: ' . $exception->getMessage());
     }
-} // show error
-catch (PDOException $exception) {
-    die('ERROR: ' . $exception->getMessage());
 }
 ?>
+
+
+<!--HTML-->
+
 
 <div class="container">
     <!-- container -->
@@ -180,8 +175,6 @@ catch (PDOException $exception) {
     </div>
     <div class="row">
         <div class="col-12">
-
-
             <!--we have our html form here where new record information can be updated-->
             <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"] . "?id={$id}"); ?>" method="POST">
                 <div class="row">
@@ -237,77 +230,77 @@ catch (PDOException $exception) {
                     </div>
 
                     <!--English-->
-                     <div class="col-6 english-data-section">
-                    <table class='table table-hover table-responsive table-bordered'>
-                        <h3> English</h3>
-                        <tr>
-                            <td>Name</td>
-                            <td><input type="text" name="name_english"
-                                       value="<?php echo htmlspecialchars($name_eng, ENT_QUOTES); ?>"
-                                       class='form-control'/></td>
-                        </tr>
-                        <tr>
-                            <div class="form-group">
-                                <label for="short_descr">Short Description</label>
-                                <textarea type="text" name="short_descr_english" rows="2"
-                                          class="form-control"><?php echo htmlspecialchars($description_eng, ENT_QUOTES); ?></textarea></td>
-                            </div>
-                        </tr>
-                        <tr>
-                            <div class="form-group">
-                                <label for="short_descr">Long Description</label>
-                                <textarea type="text" name="long_descr_english" rows="4"
-                                          class="form-control"><?php echo htmlspecialchars($descriptionLong_eng, ENT_QUOTES); ?></textarea></td>
-                                </textarea>
-                            </div>
-                        </tr>
-                    </table>
-                </div>
+                    <div class="col-6 english-data-section">
+                        <table class='table table-hover table-responsive table-bordered'>
+                            <h3> English</h3>
+                            <tr>
+                                <td>Name</td>
+                                <td><input type="text" name="name_english"
+                                           value="<?php echo htmlspecialchars($name_eng, ENT_QUOTES); ?>"
+                                           class='form-control'/></td>
+                            </tr>
+                            <tr>
+                                <div class="form-group">
+                                    <label for="short_descr">Short Description</label>
+                                    <textarea type="text" name="short_descr_english" rows="2"
+                                              class="form-control"><?php echo htmlspecialchars($description_eng, ENT_QUOTES); ?></textarea></td>
+                                </div>
+                            </tr>
+                            <tr>
+                                <div class="form-group">
+                                    <label for="short_descr">Long Description</label>
+                                    <textarea type="text" name="long_descr_english" rows="4"
+                                              class="form-control"><?php echo htmlspecialchars($descriptionLong_eng, ENT_QUOTES); ?></textarea></td>
+                                    </textarea>
+                                </div>
+                            </tr>
+                        </table>
+                    </div>
 
                     <!--Norge -->
-                     <div class="col-6 norwegian-data-section">
-                    <table class='table table-hover table-responsive table-bordered'>
-                        <h3> Norwegian</h3>
+                    <div class="col-6 norwegian-data-section">
+                        <table class='table table-hover table-responsive table-bordered'>
+                            <h3> Norwegian</h3>
 
-                        <tr>
-                            <td>Name</td>
-                            <td><input type='text' name='name_norw'
-                                       value="<?php echo htmlspecialchars($name_no, ENT_QUOTES); ?>"
-                                       class='form-control'/></td>
-                        </tr>
-                        <tr>
-                            <div class="form-group">
-                                <label for="short_descr">Short Description</label>
-                                <textarea type="text" name="short_descr_norw" rows="2"
-                                          class="form-control"><?php echo htmlspecialchars($description_no, ENT_QUOTES); ?></textarea></td>
-                            </div>
-                        </tr>
-                        <tr>
-                            <div class="form-group">
-                                <label for="short_descr">Long Description</label>
-                                <textarea type="text" name="long_descr_norw" rows="4"
-                                          class="form-control"><?php echo htmlspecialchars($descriptionLong_no, ENT_QUOTES); ?></textarea></td>
-                                </textarea>
-                            </div>
-                        </tr>
-                    </table>
-                </div>
+                            <tr>
+                                <td>Name</td>
+                                <td><input type='text' name='name_norw'
+                                           value="<?php echo htmlspecialchars($name_no, ENT_QUOTES); ?>"
+                                           class='form-control'/></td>
+                            </tr>
+                            <tr>
+                                <div class="form-group">
+                                    <label for="short_descr">Short Description</label>
+                                    <textarea type="text" name="short_descr_norw" rows="2"
+                                              class="form-control"><?php echo htmlspecialchars($description_no, ENT_QUOTES); ?></textarea></td>
+                                </div>
+                            </tr>
+                            <tr>
+                                <div class="form-group">
+                                    <label for="short_descr">Long Description</label>
+                                    <textarea type="text" name="long_descr_norw" rows="4"
+                                              class="form-control"><?php echo htmlspecialchars($descriptionLong_no, ENT_QUOTES); ?></textarea></td>
+                                    </textarea>
+                                </div>
+                            </tr>
+                        </table>
+                    </div>
 
                     <!--Button SUBMIT-->
-                      <div class="col-lg-6 right">
-                    <table class='table table-hover table-responsive table-bordered'>
-                        <tr>
-                            <td>
-                                <input type='submit' value='Save Changes' class='btn btn-primary'
-                                       name="update_btn"/>
-                                <a href='index.php' class='btn btn-danger'>Back to read products</a>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
+                    <div class="col-lg-6 right">
+                        <table class='table table-hover table-responsive table-bordered'>
+                            <tr>
+                                <td>
+                                    <input type='submit' value='Save Changes' class='btn btn-primary'
+                                           name="update_btn"/>
+                                    <a href='index.php' class='btn btn-danger'>Back to read products</a>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
 
                 </div>
             </form>
+        </div>
     </div>
-</div>
-<?php require 'footer.php'; ?>
+    <?php require 'footer.php'; ?>
