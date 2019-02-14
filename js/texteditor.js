b62329205add16425fe074ee588038ecc338d030
nikita ananjevs <40365689+NikitaAnanjev@users.noreply.github.com>
2019-02-14 01:22:10 +0100

    Update texteditor.js

--- js/texteditor.js
+++ js/texteditor.js
@@ -1,7 +1,40 @@
 // SCRIPT FOR TEXT EDITOR
+// SCRIPT FOR TEXT EDITOR
 
-var textarea = document.body.appendChild( document.createElement( 'textarea' ) );
-CKEDITOR.replace( textarea ,{
+
+CKEDITOR.replace( 'short_descr',{
+    language: 'da',
+    height: '100',
+    uiColor: '#9AB8F3',
+    toolbarCanCollapse: 'true'
+});
+
+CKEDITOR.replace( 'long_descr' ,{
+    language: 'da',
+    height: '100',
+    uiColor: '#9AB8F3',
+    toolbarCanCollapse: 'true'
+});
+
+CKEDITOR.replace( 'short_descr_norw',{
+    language: 'da',
+    height: '100',
+    uiColor: '#9AB8F3',
+    toolbarCanCollapse: 'true'
+});
+CKEDITOR.replace( 'long_descr_norw',{
+    language: 'da',
+    height: '100',
+    uiColor: '#9AB8F3',
+    toolbarCanCollapse: 'true'
+});
+CKEDITOR.replace( 'short_descr_english',{
+    language: 'da',
+    height: '100',
+    uiColor: '#9AB8F3',
+    toolbarCanCollapse: 'true'
+});
+CKEDITOR.replace( 'long_descr_english',{
     language: 'da',
     height: '100',
     uiColor: '#9AB8F3',
