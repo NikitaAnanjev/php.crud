c0f233e4f33b56536e3849d04b76a6ea41b6aa1d
nikita ananjevs <40365689+NikitaAnanjev@users.noreply.github.com>
2019-02-14 03:21:51 +0100

    Update update.php

--- update.php
+++ update.php
@@ -7,8 +7,10 @@ $id = isset($_GET['id']) ? $_GET['id'] : die('ERROR: Record ID not found.');
 //include database connection
 include 'config/database.php';
 
+
 // READ DATA FROM DATABASE
 try {
+
     $query = "SELECT * FROM products_description INNER JOIN products ON products.products_id = products_description.products_id WHERE products_description.products_id = '$id'";
     $stmt = $con->prepare($query);
     $stmt->execute();
@@ -45,17 +47,23 @@ try {
             $lang_name_no =  'Norwegian';
         }
     }
+
+    // this is the first question mark
+//    $stmt->bindParam(1, $id);
     $price = $products_price;
     $reference = $products_reference;
+
+
 } // show error
 catch (PDOException $exception) {
     die('ERROR: ' . $exception->getMessage());
 }
-
-
 //UPDATE DATABASE
 if ($_POST) {
+//if (isset($_POST['update_btn'])) {
     try {
+
+
         $query = "UPDATE
     products_description
 JOIN products ON products_description.products_id = products.products_id
@@ -67,9 +75,12 @@ SET
     products.products_price =:price
 WHERE
     products_description.languages_id = '1' AND products_description.products_id = :id;";
+
+
         $stmt = $con->prepare($query);
 
 //      UPDATE ENGLISH DATA
+
         $query_eng = "UPDATE
     products_description
 SET
@@ -80,7 +91,9 @@ WHERE
     products_description.languages_id = '2' AND products_id=:id";
         $stmt_en = $con->prepare($query_eng);
 
-//      UPDATE NORWEGIAN
+//
+//////      UPDATE NORWEGIAN
+////
         $query_nor = "UPDATE
     products_description
 SET
@@ -89,6 +102,7 @@ SET
     products_description_description=:long_descr_no
 WHERE
     products_description.languages_id = '3' AND   products_description.products_id=:id";
+
 
         $stmt_no = $con->prepare($query_nor);
         $price = htmlspecialchars(strip_tags($_POST['price']));
@@ -125,10 +139,13 @@ WHERE
         $stmt_no->bindParam(':long_descr_no', $descriptionLong_no);
         $stmt_no->bindParam(':id', $id);
 
+
         $stmt->execute();
         $stmt_en->execute();
         $stmt_no->execute();
 
+
+//        if ($stmt->execute()) {
         if ($stmt->execute() && $stmt_en->execute() && $stmt_no->execute()) {
 
             echo "<div class='alert alert-success text-center'>Tillykke! Optagelsen blev opdateret.</div>";
@@ -152,7 +169,9 @@ WHERE
             <!--we have our html form here where new record information can be updated-->
             <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"] . "?id={$id}"); ?>" method="POST">
                 <div class="row">
-                    <div class="col-4 main-data-section">
+
+
+                    <div class="col-12 col-md-4 main-data-section">
                         <table class="table table-hover table-bordered">
                             <h3>Oplysningen</h3>
                             <tr>
@@ -170,13 +189,15 @@ WHERE
                         </table>
                     </div>
 
-                    <div class="col-4 offset-4 text-right">
+                    <div class="col-12 col-md-4 offset-md-4 text-right">
                         <a href="index.php" class="btn btn-danger"><?php echo $lable_back; ?></a>
                         <input type='submit' value='Opdater' class='btn btn-primary' name="update_btn"/>
+
                     </div>
 
                     <!--Dansk-->
-                    <div class="col-4 dansk-data-section">
+                    <div class="col-12 col-md-4 dansk-data-section">
+
                             <h3> Dansk</h3>
                             <tr>
                                 <label><?php echo $lable_prod_name; ?></label>
@@ -204,7 +225,7 @@ WHERE
                     </div>
 
                     <!--English-->
-                    <div class="col-4 english-data-section">
+                    <div class="col-12 col-md-4 english-data-section">
                             <h3> <?php echo $lang_name_eng; ?></h3>
                             <tr>
                                 <label><?php echo $lable_prod_name; ?></label>
@@ -227,10 +248,11 @@ WHERE
                                     </textarea>
                                 </div>
                             </tr>
-                      </div>
+
+                    </div>
 
                     <!--Norge -->
-                    <div class="col-4 norwegian-data-section">
+                    <div class="col-12 col-md-4 norwegian-data-section">
                             <h3> <?php echo $lang_name_no; ?></h3>
                             <tr>
                                 <label><?php echo $lable_prod_name; ?></label>
